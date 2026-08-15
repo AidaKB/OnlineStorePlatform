4a192a4962404485fbd31bc8c27cf37479244268
Aida Beheshti <aida.beheshti81@gmail.com>
2026-08-15 15:29:16 +0330

    add products useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
+
 
 
 function App() {
   
-
+    const [products, setProducts] = useState([{ name: "phone", price: 15222 },
+        { name: "laptop", price: 878745 }]);
+    const addProduct = () => {
+        setProducts(prevState => [...prevState, { name: "product" + (prevState.length + 1), price: prevState[prevState.length - 1].price + 100 }])
+    }
     return (
-        <div style={{fontSize : '1.8rem'}} > Restore</div>
+        <div>
+            <h1 style={{ color: 'red' }}>Re Store</h1>
+            <ul>
+                {products.map((item , index) => (
+                    <li key={index}>{item.name} - {item.price}</li>
+                ))}
+            </ul>
+            <button onClick={() => addProduct()}>Add Product</button>
+        </div>
   )
 }
 
